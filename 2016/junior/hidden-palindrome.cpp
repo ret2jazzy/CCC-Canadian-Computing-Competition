d583df8198b9b76d1bc78d1a95d5ecad30894ada
Drigg3r <drigg3r@yandex.com>
2016-10-01 19:33:22 -0500

    Update hidden-palindrome.cpp

--- 2016/junior/hidden-palindrome.cpp
+++ 2016/junior/hidden-palindrome.cpp
@@ -17,39 +17,24 @@ bool desc(string l, string m){return l.size() > m.size();}
 
 void CheckPal()
 {
-	string nur = input;
-	ans.push_back(nur);
-	int m = nur.size()/2;
-	for (int i = 0; i < m ; ++i){
-		nur.erase(nur.begin());
-		ans.push_back(nur);
-		nur.pop_back();
-		ans.push_back(nur);
-	}
-	string nur2 = input;
-	for(int i = 0; i < nur2.size();){
-		nur2.erase(nur2.begin());
-		ans.push_back(nur2);
-	}
-	string nur3 = input;
-	for(int i = 0; i < nur3.size();){
-		nur3.pop_back();
-		ans.push_back(nur3);
+	for(int i = 0; i < input.size(); ++i){
+		for(int t = i; t < input.size(); ++t){
+			ans.push_back(input.substr(i,t));
+		}
 	}
 	sort(ans.begin(), ans.end(), desc);
 }
 
-string Checkfin()
+int Checkfin()
 {
 	for (auto l : ans){
 		string m = l;
 		reverse(l.begin(), l.end());
 		if(l == m){
-			return l;
-			break;
+			return l.size();
 		}
 	}
-	return NULL;
+	return 0;
 }
 
 int main()
